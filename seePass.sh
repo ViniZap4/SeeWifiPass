78938f0daaba722b24822c806da93c7800732075
Vinicius Santos <vinicius.zapala@veredaeducacao.com>
2023-10-11 08:49:35 -0300

    refactor: set echo to jump lines and text.
     Changes to be committed:
    	modified:   seePass.sh

--- seePass.sh
+++ seePass.sh
@@ -3,10 +3,12 @@
 # Get the list of Wi-Fi network names
 networksetup -listpreferredwirelessnetworks en0
 
+echo -e "\n"
+
 #get network that want get pass
 read -p "put name of network:" network
 
-echo ""
+echo -e "\nThe password is:"
 
 #show Pass
 security find-generic-password -wa "$network"
